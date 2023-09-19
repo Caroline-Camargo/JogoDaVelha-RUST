a89692d08a073a372e3db7bc0927974924de6908
Caroline Souza Camargo <carolinecamargo012@gmail.com>
2023-09-19 20:38:20 -0300

    Adicionando melhorias

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# JogoDaVelha-RUST
-Jogo da Velha desenvolvido para a disciplina de tópicos em computação VII
+# Jogo da Velha em Rust com ggez :video_game: :older_woman:
+Este é um simples jogo da velha implementado em Rust usando a biblioteca ggez para a disciplina de Tópicos Especiais em Computação VII. O jogo permite que dois jogadores, representados como "X" e "O", joguem alternadamente em um tabuleiro 3x3 até que um jogador vença ou o jogo termine em empate.
+
+## Como Jogar :warning:
+- O jogador "X" sempre começa.
+- Clique em um quadrado vazio para fazer sua jogada.
+- O objetivo é formar uma linha horizontal, vertical ou diagonal com três das suas marcações consecutivas ("X" ou "O").
+- O jogo termina quando um jogador vence ou o tabuleiro fica completamente preenchido.
+- Quando isso acontece o jogo automaticamente reinicia 
+
+## Como Executar :arrow_forward:
+
+   ```sh
+   cargo run
--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,49 @@ use ggez;
 use ggez::event; 
 use ggez::graphics; 
 use ggez::{Context, GameResult}; 
-use ggez::nalgebra as na; 
+use nalgebra as na;
 
 // Constantes que definem o tamanho dos quadrados do jogo da velha e o espaço entre eles.
 const LARGURA_QUADRADO: f32 = 80.0;
-const ALTURA_QUADRADO: f32 = 80.0;
 const ESPACO_ENTRE_QUADRADOS: f32 = 10.0;
 
 // Definição da estrutura de dados que representa o estado do jogo.
 struct EstadoJogo {
-    posicoes_quadrados: Vec<Vec<na::Point2<f32>>>, // Matriz que armazena as posições dos quadrados no jogo da velha (pontos bidimensionais: biblioteca nalgebra)
-    marcacoes_quadrados: Vec<Vec<Option<String>>>, // Matriz com a descrição do conteúdo dos quadrados (X, O ou vazio).
+    posicao_bloco: Vec<Vec<na::Point2<f32>>>, // Matriz que armazena as posições dos blocos no jogo da velha (pontos bidimensionais: biblioteca nalgebra)
+    conteudo_bloco: Vec<Vec<Option<String>>>, // Matriz com a descrição do conteúdo dos quadrados (X, O ou vazio).
     jogador_atual: String, // Jogador atual (X ou O).
 }
 
 impl EstadoJogo {
     // Construtor para criar um novo estado de jogo.
     fn novo() -> GameResult<EstadoJogo> {
-        // Inicializa as posições dos quadrados e as marcações como vazias.
+        // Inicializa as posições onde os blocos devem começar.
         let x = 100.0;
         let y = 100.0;
 
-        let mut posicoes_quadrados = Vec::with_capacity(3);  // Aloca 3 posições para o vetor
-        let mut marcacoes_quadrados = Vec::with_capacity(3);
+        let mut posicao_bloco = Vec::with_capacity(3);  // Aloca 3 posições para o vetor
+        let mut conteudo_bloco = Vec::with_capacity(3);
 
         for linha in 0..3 { // Percorre as linhas
-            let mut linha_quadrados = Vec::with_capacity(3);
-            let mut linha_marcacoes = Vec::with_capacity(3);
+            let mut linha_tempposicao = Vec::with_capacity(3);
+            let mut linha_tempconteudo = Vec::with_capacity(3);
 
+            let y_bloco = y + linha as f32 * (LARGURA_QUADRADO + ESPACO_ENTRE_QUADRADOS); // Cordenada y do quadrado
             for coluna in 0..3 { // Percorre as colunas
-                let x_quadrado = x + coluna as f32 * (LARGURA_QUADRADO + ESPACO_ENTRE_QUADRADOS);
-                let y_quadrado = y + linha as f32 * (ALTURA_QUADRADO + ESPACO_ENTRE_QUADRADOS);
-
-                linha_quadrados.push(na::Point2::new(x_quadrado, y_quadrado));
-                linha_marcacoes.push(None);
+                let x_bloco = x + coluna as f32 * (LARGURA_QUADRADO + ESPACO_ENTRE_QUADRADOS); // Cordenada x do quadrado
+                
+                linha_tempposicao.push(na::Point2::new(x_bloco, y_bloco));
+                linha_tempconteudo.push(None);
             }
 
-            posicoes_quadrados.push(linha_quadrados);
-            marcacoes_quadrados.push(linha_marcacoes);
+            posicao_bloco.push(linha_tempposicao);
+            conteudo_bloco.push(linha_tempconteudo);
         }
 
         // Retorna o estado inicializado com o jogador atual como "X".
         Ok(EstadoJogo {
-            posicoes_quadrados,
-            marcacoes_quadrados,
+            posicao_bloco,
+            conteudo_bloco,
             jogador_atual: "X".to_string(),
         })
     }
@@ -55,13 +54,13 @@ impl EstadoJogo {
     fn verificar_clique_quadrado(&self, x: f32, y: f32) -> Option<(usize, usize)> {
         for linha in 0..3 {
             for coluna in 0..3 {
-                let posicao_quadrado = self.posicoes_quadrados[linha][coluna];
-                let dentro_x = x >= posicao_quadrado.x - LARGURA_QUADRADO * 0.5
-                    && x <= posicao_quadrado.x + LARGURA_QUADRADO * 0.5;
-                let dentro_y = y >= posicao_quadrado.y - ALTURA_QUADRADO * 0.5
-                    && y <= posicao_quadrado.y + ALTURA_QUADRADO * 0.5;
+                let posicao_bloco = self.posicao_bloco[linha][coluna]; // Coordenadas do centro do bloco
+                let dentro_x = x >= posicao_bloco.x - LARGURA_QUADRADO * 0.5
+                    && x <= posicao_bloco.x + LARGURA_QUADRADO * 0.5; // Verifica coordenada mínima e máxima na horizontal do bloco
+                let dentro_y = y >= posicao_bloco.y - LARGURA_QUADRADO * 0.5
+                    && y <= posicao_bloco.y + LARGURA_QUADRADO * 0.5; // Verifica coordenada mínima e máxima na vertical do bloco
                 
-                if dentro_x && dentro_y && self.marcacoes_quadrados[linha][coluna].is_none() {
+                if dentro_x && dentro_y && self.conteudo_bloco[linha][coluna].is_none() {
                     return Some((linha, coluna));
                 }
             }
@@ -69,17 +68,17 @@ impl EstadoJogo {
         None
     }
 
-    // Verifica se o jogador atual venceu com base na última marcação feita.
+    // Verifica se o jogador atual venceu 
     fn verificar_vitoria(&self, linha: usize, coluna: usize) -> bool {
         // Obtém o jogador atual com base na última marcação na posição especificada.
-        let jogador_atual = match &self.marcacoes_quadrados[linha][coluna] {
+        let jogador_atual = match &self.conteudo_bloco[linha][coluna] {
             Some(jogador) => jogador,
-            None => return false, // Retorna false se não houver marcação.
+            None => return false, // Retorna false se não houver marcação no bloco atual.
         };
     
         // Verifica se o jogador atual venceu na linha onde a última marcação foi feita.
         for i in 0..3 {
-            if self.marcacoes_quadrados[linha][i].as_ref() != Some(jogador_atual) {
+            if self.conteudo_bloco[linha][i].as_ref() != Some(jogador_atual) {
                 break; // Se uma marcação não corresponder, sai do loop.
             }
             if i == 2 {
@@ -89,7 +88,7 @@ impl EstadoJogo {
     
         // Verifica se o jogador atual venceu na coluna onde a última marcação foi feita.
         for i in 0..3 {
-            if self.marcacoes_quadrados[i][coluna].as_ref() != Some(jogador_atual) {
+            if self.conteudo_bloco[i][coluna].as_ref() != Some(jogador_atual) {
                 break; // Se uma marcação não corresponder, sai do loop.
             }
             if i == 2 {
@@ -100,7 +99,7 @@ impl EstadoJogo {
         // Verifica se o jogador atual venceu na diagonal principal.
         if linha == coluna {
             for i in 0..3 {
-                if self.marcacoes_quadrados[i][i].as_ref() != Some(jogador_atual) {
+                if self.conteudo_bloco[i][i].as_ref() != Some(jogador_atual) {
                     break; // Se uma marcação não corresponder, sai do loop.
                 }
                 if i == 2 {
@@ -112,7 +111,7 @@ impl EstadoJogo {
         // Verifica se o jogador atual venceu na diagonal secundária.
         if linha + coluna == 2 {
             for i in 0..3 {
-                if self.marcacoes_quadrados[i][2 - i].as_ref() != Some(jogador_atual) {
+                if self.conteudo_bloco[i][2 - i].as_ref() != Some(jogador_atual) {
                     break; // Se uma marcação não corresponder, sai do loop.
                 }
                 if i == 2 {
@@ -126,9 +125,9 @@ impl EstadoJogo {
     }
     
 
-    // Verifica se o jogo terminou em empate.
+    // Verifica se o jogo terminou em empate (todos blocos devem ter sido preenchidos).
     fn verificar_empate(&self) -> bool {
-        for linha in &self.marcacoes_quadrados {
+        for linha in &self.conteudo_bloco {
             for marcacao in linha {
                 if marcacao.is_none() {
                     return false;
@@ -138,16 +137,17 @@ impl EstadoJogo {
         true
     }
 
-    // Reinicia o jogo para um novo jogo vazio.
+    // Reinicia o jogo para um novo jogo vazio (X é o jogador inicial).
     fn reiniciar(&mut self) {
         for linha in 0..3 {
             for coluna in 0..3 {
-                self.marcacoes_quadrados[linha][coluna] = None;
+                self.conteudo_bloco[linha][coluna] = None;
             }
         }
         self.jogador_atual = "X".to_string();
     }
 }
+
 
 impl event::EventHandler for EstadoJogo {
     // Método chamado a cada atualização do jogo.
@@ -173,12 +173,12 @@ impl event::EventHandler for EstadoJogo {
         // Itera sobre os quadrados do tabuleiro e desenha cada um.
         for linha in 0..3 {
             for coluna in 0..3 {
-                let posicao = self.posicoes_quadrados[linha][coluna];
+                let posicao = self.posicao_bloco[linha][coluna];
                 let retangulo_quadrado = graphics::Rect::new(
-                    -LARGURA_QUADRADO * 0.5,
-                    -ALTURA_QUADRADO * 0.5,
+                    LARGURA_QUADRADO * -0.5,
+                    LARGURA_QUADRADO * -0.5,
                     LARGURA_QUADRADO,
-                    ALTURA_QUADRADO,
+                    LARGURA_QUADRADO,
                 );
                 
                 // Cria uma malha (retângulo) representando o quadrado com a cor definida.
@@ -190,28 +190,27 @@ impl event::EventHandler for EstadoJogo {
                 )?;
     
                 let mut parametro_desenho_quadrado = graphics::DrawParam::default();
-                parametro_desenho_quadrado.dest = posicao.into();
+                parametro_desenho_quadrado.dest.x = posicao.x;
+                parametro_desenho_quadrado.dest.y = posicao.y;
+
     
                 // Desenha o quadrado na posição atual.
                 graphics::draw(ctx, &malha_quadrado, parametro_desenho_quadrado)?;
     
                 // Desenha as marcações (X ou O) nos quadrados se existirem.
-                if let Some(marcacao) = &self.marcacoes_quadrados[linha][coluna] {
+                if let Some(marcacao) = &self.conteudo_bloco[linha][coluna] {
                     // Cria um texto com a marcação.
-                    let marcacao_texto = graphics::Text::new(marcacao.clone());
+                    let texto = graphics::Text::new(marcacao.clone());
                     
                     // Calcula a posição para a marcação (levemente deslocada do centro do quadrado).
                     let posicao_marcacao = na::Point2::new(posicao.x - 10.0, posicao.y - 10.0);
-    
-                    // Define a cor das marcações como branco.
-                    let cor_marcacao = graphics::Color::new(1.0, 1.0, 1.0, 1.0);
-    
+        
                     let mut parametro_desenho_marcacao = graphics::DrawParam::default();
-                    parametro_desenho_marcacao.dest = posicao_marcacao.into();
-                    parametro_desenho_marcacao.color = cor_marcacao;
+                    parametro_desenho_marcacao.dest.x = posicao_marcacao.x;
+                    parametro_desenho_marcacao.dest.y = posicao_marcacao.y;
     
                     // Desenha a marcação (X ou O) na posição calculada.
-                    graphics::draw(ctx, &marcacao_texto, parametro_desenho_marcacao)?;
+                    graphics::draw(ctx, &texto, parametro_desenho_marcacao)?;
                 }
             }
         }
@@ -221,7 +220,8 @@ impl event::EventHandler for EstadoJogo {
         let titulo_posicao = na::Point2::new(140.0, 20.0);
     
         let mut parametro_desenho_titulo = graphics::DrawParam::default();
-        parametro_desenho_titulo.dest = titulo_posicao.into();
+        parametro_desenho_titulo.dest.x = titulo_posicao.x;
+        parametro_desenho_titulo.dest.y = titulo_posicao.y;
         parametro_desenho_titulo.color = graphics::BLACK;
     
         // Desenha o título na posição especificada.
@@ -240,9 +240,9 @@ impl event::EventHandler for EstadoJogo {
             // Chama a função verificar clique para determinar se o clique ocorreu em um quadrado válido.
             if let Some((linha, coluna)) = self.verificar_clique_quadrado(x, y) {
                 // Verifica se o quadrado clicado está vazio (sem marcação).
-                if self.marcacoes_quadrados[linha][coluna].is_none() {
+                if self.conteudo_bloco[linha][coluna].is_none() {
                     // Marca o quadrado com o símbolo do jogador atual (X ou O).
-                    self.marcacoes_quadrados[linha][coluna] = Some(self.jogador_atual.clone());
+                    self.conteudo_bloco[linha][coluna] = Some(self.jogador_atual.clone());
     
                     // Verifica se o jogador atual venceu após fazer a marcação.
                     if self.verificar_vitoria(linha, coluna) {
@@ -275,7 +275,7 @@ impl event::EventHandler for EstadoJogo {
 fn main() -> GameResult {
     // Calcula a largura e altura da tela com base nas constantes.
     let largura_tela = 3.0 * (LARGURA_QUADRADO + ESPACO_ENTRE_QUADRADOS) + 100.0;
-    let altura_tela = 3.0 * (ALTURA_QUADRADO + ESPACO_ENTRE_QUADRADOS) + 100.0;
+    let altura_tela = 3.0 * (LARGURA_QUADRADO + ESPACO_ENTRE_QUADRADOS) + 100.0;
 
     // Configura o contexto do jogo com um título e dimensões.
     let cb = ggez::ContextBuilder::new("jogo_da_velha", "Caroline")
